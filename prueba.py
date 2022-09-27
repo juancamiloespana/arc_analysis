e7d73fda1c5f127cd09cd54fcd804bd2a908134e
susanabarrerah <susana.barrera@udea.edu.co>
2022-09-27 09:28:32 -0500

    Comentarios

--- prueba.py
+++ prueba.py
@@ -39,17 +39,23 @@ df_ffkpi.describe()
 
 #Histogramas
 plt.hist(df_ffkpi['Costo'])
+#Dispersión y variabilidad
 plt.hist(df_ffkpi['Arcos_faltantes'], ec="black")
+#está centrado. ¿Se debe a la simulación?
 plt.hist(df_ffkpi['Costo_ventas_perdidas'], ec="black")
+#Dispersión y variabilidad
 plt.hist(df_ffkpi['Ventas_perdidas'], ec="black")
+#Dispersión y variabilidad
 plt.hist(df_ffkpi['Costo_real_de_la_CS'], ec="black")
+#leve sesgo a la derecha
 plt.hist(df_ffkpi['Tiempo_tot_esc'], ec="black")
 
 #Gráficos de dispersión
 df_ffkpi
 sns.scatterplot(data=df_ffkpi, x='Costo_ventas_perdidas', y='Arcos_faltantes') 
-sns.scatterplot(data=df_ffkpi, x='Arcos_faltantes', y='Costo_ventas_perdidas')
 sns.scatterplot(data=df_ffkpi, x='Arcos_faltantes', y='Ventas_perdidas')
+sns.scatterplot(data=df_ffkpi, x='Arcos_faltantes', y='Costo_real_de_la_CS')
+
 #Parece no haber relación entre los arcos faltantes y el costo de las ventas perdidas
 #No es la cantidad de arcos la que afecta las ventas perdidas. Debe haber arcos criticos
 
@@ -57,5 +63,11 @@ sns.scatterplot(data=df_ffkpi, x='Tiempo_tot_esc', y='Arcos_faltantes')
 sns.scatterplot(data=df_ffkpi, x='Costo_ventas_perdidas', y='Costo_real_de_la_CS')
 sns.scatterplot(data=df_ffkpi, x='Costo_ventas_perdidas', y='Ventas_perdidas')
 sns.scatterplot(data=df_ffkpi, x='Ventas_perdidas', y='Costo_real_de_la_CS')
+sns.scatterplot(data=df_ffkpi, x='Costo', y='Costo_real_de_la_CS')
+
+#Gráfico de correlación
 figure(figsize=(7, 5), dpi=80);
 sns.heatmap(df_ffkpi.corr(), annot = True, cmap="Blues",center=0)
+#La correlación entre costo y costo de ventas perdidad/ ventas perdidas es muy fuerte
+#La correlación entre el costo y el costo real de la CS es inversamente proporcional aligual que las ventas perdidas y el costo de ventas perdidas
+#La correlación entre los arcos faltantes y el costo real de la CS es inversamente proporcional, aunque no es muy fuerte
